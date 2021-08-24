62389af89b9eccfb7592125493577bde765d1b1c
yeonsuan <sweetsuan@naver.com>
2021-08-24 13:50:47 +0900

    redux firestore

--- bucket-list/src/redux/modules/bucket.js
+++ bucket-list/src/redux/modules/bucket.js
@@ -1,152 +1,156 @@
-//bucket.js
-import { firestore } from "../../firebase";
+import React from "react";
+import { withRouter } from "react-router";
+import { Route, Switch } from "react-router-dom";
 
-const bucket_db = firestore.collection("bucket")
+// import [컴포넌트 명] from [컴포넌트가 있는 파일경로];
+import BucketList from "./BucketList";
+import styled from "styled-components";
+import Detail from "./Detail";
+import NotFound from "./NotFound";
 
-//Actions
+// 리덕스 스토어와 연결하기 위해 connect라는 친구를 호출할게요!
+import { connect } from "react-redux";
+// 리덕스 모듈에서 (bucket 모듈에서) 액션 생성 함수 두개를 가져올게요!
+import {
+  loadBucket,
+  createBucket,
+  loadBucketFB,
+  addBucketFB,
+} from "./redux/modules/bucket";
+import Progress from "./Progress";
 
-const LOAD = "bucket/LOAD";
-const CREATE = "bucket/CREATE";
-const DELETE = "bucket/DELETE";
-const UPDATE = "bucket/UPDATE";
+import Spinner from "./Spinner";
+// firestore 가져오기
+import { firestore } from "./firebase";
 
-const initialState = {
-  //   list: ["영화관 가기", "매일 책읽기", "수영 배우기"],
-  list: [
-    { text: "영화관 가기", completed: false },
-    { text: "매일 책읽기", completed: false },
-    { text: "수영 배우기", completed: false },
-  ],
-};
+// 이 함수는 스토어가 가진 상태값을 props로 받아오기 위한 함수예요.
+const mapStateTopProps = (state) => ({
+  bucket_list: state.bucket.list,
+  is_loaded: state.bucket.is_loaded,
+});
 
-// Action Creators
-export const loadBucket = (bucket) => {
-  return { type: LOAD, bucket };
-};
+// 이 함수는 값을 변화시키기 위한 액션 생성 함수를 props로 받아오기 위한 함수예요.
+const mapDispatchToProps = (dispatch) => ({
+  load: () => {
+    dispatch(loadBucketFB());
+  },
+  create: (new_item) => {
+    console.log(new_item);
+    dispatch(addBucketFB(new_item));
+  },
+});
 
-export const createBucket = (bucket) => {
-  return { type: CREATE, bucket };
-};
+// 클래스형 컴포넌트는 이렇게 생겼습니다!
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    // App 컴포넌트의 state를 정의해줍니다.
+    this.state = {};
+    // ref는 이렇게 선언합니다!
+    this.text = React.createRef();
+  }
 
-export const deleteBucket = (bucket) => {
-  return { type: DELETE, bucket };
-};
+  componentDidMount() {
+    this.props.load();
+  }
 
-export const updateBucket = (bucket) => {
-  return { type: UPDATE, bucket };
-};
+  addBucketList = () => {
+    const new_item = this.text.current.value;
+    this.props.create(new_item);
+  };
 
+  // 랜더 함수 안에 리액트 엘리먼트를 넣어줍니다!
+  render() {
+    // 콘솔로 확인해요!
+    console.log(this.props.is_loaded);
+    return (
+      <div className="App">
+        <Container>
+          <Title>내 버킷리스트</Title>
+          {/* firestore에서 데이터를 가져온 후에만 페이지를 보여줄거예요!  */}
+          {!this.props.is_loaded ? (
+            <Spinner />
+          ) : (
+            <React.Fragment>
+              <Progress />
+              <Line />
+              <Switch>
+                <Route path="/" exact component={BucketList} />
+                <Route path="/detail/:index" component={Detail} />
+                <Route component={NotFound} />
+              </Switch>
+            </React.Fragment>
+          )}
+        </Container>
+        <Input>
+          <input type="text" ref={this.text} />
+          <button onClick={this.addBucketList}>추가하기</button>
+        </Input>
 
-export const loadBucketFB = () => {
-  return function (dispatch) {
-
-    bucket_db.get().then((docs) => {
-      let bucket_data = [];
-
-      docs.forEach((doc) => {
-        if (doc.exists) {
-          bucket_data = [...bucket_data, { id: doc.id, ...doc.data() }]
-        }
-      })
-
-      console.log(bucket_data);
-      dispatch(loadBucket(bucket_data));
-    });
+        <button
+          onClick={() => {
+            window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+          }}
+        >
+          위로가기
+        </button>
+      </div>
+    );
   }
 }
 
-
-export const addBucketFB = (bucket) => {
-  return function (dispatch) {
-    let bucket_data = { text: bucket, completed: false };
-
-    bucket_db.add(bucket_data).then(docRef => {
-      bucket_data = { ...bucket_data, id: docRef.id };
-      dispatch(createBucket(bucket_data));
-    })
+const Input = styled.div`
+  max-width: 350px;
+  min-height: 10vh;
+  background-color: #fff;
+  padding: 16px;
+  margin: 20px auto;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  & > * {
+    padding: 5px;
   }
-}
 
-export const updatteBucketFB = (bucket) => {
-  return function (dispatch, getState) {
-    const _bucket_data = getState().bucket.list[bucket];
-
-    
-
-    let bucket_data = { ..._bucket_data, completed: true };
-
-    if(!bucket_data.id){
-      return;
+  & input {
+    border-radius: 5px;
+    margin-right: 10px;
+    border: 1px solid #888;
+    width: 70%;
+    &:focus {
+      border: 1px solid #a673ff;
     }
-
-    bucket_db.doc(bucket_data.id).update(bucket_data).then(docRef => {
-      dispatch(updateBucket(bucket));
-    }).catch(error => {
-      console.log(error)
-    });
   }
-}
 
-export const deleteBucketFB = (bucket) => {
-  return function (dispatch, getState) {
-    const _bucket_data = getState().bucket.list[bucket];
-
-    if(_bucket_data.id){
-      return;
-    }
-
-    bucket_db.doc(_bucket_data.id).delete().then(docRef => {
-      dispatch(deleteBucket(bucket));
-    }).catch(error => {
-      console.log(error);
-    });
+  & button {
+    width: 25%;
+    color: #fff;
+    border: 1px solid #a673ff;
+    background-color: #a673ff;
   }
-}
+`;
 
-// Reducer
-export default function reducer(state = initialState, action) {
+const Container = styled.div`
+  max-width: 350px;
+  min-height: 60vh;
+  background-color: #fff;
+  padding: 16px;
+  margin: 20px auto;
+  border-radius: 5px;
+  border: 1px solid #ddd;
+`;
 
-  switch (action.type) {
-    // do reducer stuff
-    case "bucket/LOAD": {
-      if (action.bucket.length > 0) {
-        return { list: action.bucket };
-      }
-      return state;
-    }
+const Title = styled.h1`
+  color: #673ab7;
+  text-align: center;
+`;
 
-    case "bucket/CREATE": {
-
-      const new_bucket_list = [
-        ...state.list,
-        action.bucket,
-      ];
-      return { list: new_bucket_list };
-    }
-
-    case "bucket/DELETE": {
-      const bucket_list = state.list.filter((l, idx) => {
-        if (idx !== action.bucket) {
-          return l;
-        }
-      });
-      return { list: bucket_list };
-    }
-
-    case "bucket/UPDATE": {
-      const bucket_list = state.list.map((l, idx) => {
-        if (idx === action.bucket) {
-
-          return { ...l, completed: true };
-        }
-
-        return l;
-      });
-
-      return { list: bucket_list };
-    }
-
-    default:
-      return state;
-  }
-}
+const Line = styled.hr`
+  margin: 16px 0px;
+  border: 1px dotted #ddd;
+`;
+// withRouter 적용
+// connect로 묶어줬습니다!
+export default connect(mapStateTopProps, mapDispatchToProps)(withRouter(App));
